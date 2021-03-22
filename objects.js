327f401d6364b08ff536d16e39e222f52642f80d
Sam <samabarker@gmail.com>
2021-03-22 19:25:49 +0000

    added functions to object

--- objects.js
+++ objects.js
@@ -21,19 +21,32 @@ recipe = {
     recipeTitle: 'Lasagna',
     servings: '4',
     ingredients: ['Lasagna Sheets', 'Tomato Sauce', 'Cheese', 'Mince'],
-    directions: ['1. Cook mince into pasta sauce', '2. Place lasagna sheets into base of dish', '3. Add layer of sauce', '4. Add layer of cheese', '5. Add layer of lasagna sheets', '6. Repeat until dish is full', '7. Cook at 180C for 40 minutes']
+    directions: ['1. Cook mince into pasta sauce', '2. Place lasagna sheets into base of dish', '3. Add layer of sauce', '4. Add layer of cheese', '5. Add layer of lasagna sheets', '6. Repeat until dish is full', '7. Cook at 180C for 40 minutes'],
+    letsCook: function() {
+        console.log(`I'm hungry! Lets cook ${this.recipeTitle}!`)
+    },
+    printIngredients: function() {
+        console.log('INGREDIENTS');
+        for(i=0; i < this.ingredients.length; i++) {
+            console.log(`Ingredient ${i+1} is ${this.ingredients[i]}`);
+        };
+    }
 };
 
-// List ingredients
-console.log('INGREDIENTS');
-for(i=0; i < recipe.ingredients.length; i++) {
-    console.log(`Ingredient ${i+1} is ${recipe.ingredients[i]}`);
-};
+// // List ingredients
+// console.log('INGREDIENTS');
+// for(i=0; i < recipe.ingredients.length; i++) {
+//     console.log(`Ingredient ${i+1} is ${recipe.ingredients[i]}`);
+// };
 
-console.log('');
+// console.log('');
 
-// List steps
-console.log('DIRECTIONS');
-for(i=0; i < recipe.directions.length; i++) {
-    console.log(recipe.directions[i]);
-};
+// // List steps
+// console.log('DIRECTIONS');
+// for(i=0; i < recipe.directions.length; i++) {
+//     console.log(recipe.directions[i]);
+// };
+
+// Call letsCook
+recipe.letsCook();
+recipe.printIngredients();
